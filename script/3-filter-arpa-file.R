332e0d73dd705749371dda990fd31b2614f59f76
Xiaoyang Jin <jinxiaoyang@jinxiaoyang.cn>
2017-06-17 04:15:35 +0800

    Save uncommited changes

--- script/3-filter-arpa-file.R
+++ script/3-filter-arpa-file.R
@@ -1,6 +1,7 @@
 library(tidyverse)
 library(stringr)
 library(forcats)
+library(magrittr)
 
 model.1 <- read_tsv("lm/en.arpa.1.txt",
                     col_names = c('pro', 'word', 'back_pro'),
@@ -15,6 +16,8 @@ model.1 <- read_tsv("lm/en.arpa.1.txt",
 model.1 = model.1 %>%
     filter(row_number() <=2 | as.numeric(pro) >= -6) %T>%
     write_tsv('lm/en.arpa.1.min.txt')
+
+writeLines(sort(model.1$word), 'lm/en.arpa.1.word')
 
 model.2 = read_tsv("lm/en.arpa.2.txt",
                    col_names = c('pro', 'word', 'back_pro'),
